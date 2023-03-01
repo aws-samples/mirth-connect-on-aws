95f7aec44c24e69e9dddd0b3f3a2d48b086c04fa
Kousik Rajendran <kousikraj@gmail.com>
2023-03-01 22:00:20 +0530

    Removing future stack

--- app.py
+++ app.py
@@ -5,12 +5,8 @@
 import aws_cdk as cdk
 import cfg
 from stacks.mirth_connect_on_aws import MirthConnectStack
-from stacks.jdbc_to_csv_channel import JDBCtoCSVChannel
 
 #Initializing the stack
 app = cdk.App()
 mirth_stack = MirthConnectStack(app, cfg.APP_NAME, description=cfg.CFN_STACK_DESCRIPTION)
-jdbc_example_stack = JDBCtoCSVChannel(app, cfg.JDBC_CHANNEL_STACK_NAME, 
-                                      description=cfg.CFN_JDBC_CHANNEL_STACK_DESCRIPTION,
-                                      mirth_stack=mirth_stack)
 app.synth()
